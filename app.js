4336ac6f00b8ea49d0ebed3fad1f64d92d88e6a5
pranulkbv28 <28bvpranaav@gmail.com>
2023-09-17 03:33:16 +0530

    first js added

---
+++ app.js
@@ -0,0 +1,18 @@
+let showTaskAdd = document.getElementById("showTaskAdd");
+
+
+
+
+
+
+
+// to show the task adding card
+function showTaskInfo(){
+    // if(showTaskAdd.classList.contains("d-none")){
+    //     showTaskAdd.classList.remove("d-none");
+    // }
+    // else{
+    //     showTaskAdd.classList.add("d-none");
+    // }
+    showTaskAdd.classList.toggle("d-none");
+}
